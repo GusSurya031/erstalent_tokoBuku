d3063b94128875581e24af1c685b7447bebeb8ec
Damar Galih <damarmuhamad13@gmail.com>
2024-11-10 18:31:44 +0700

    fix: Resolved merge conflict in web.php

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,34 @@ use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\SaleController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
+})->name('home');
+
+Route::middleware(['guest'])->group(function(){
+    Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/login', [AuthenticateController::class, 'index'])->name('login');
+    Route::post('/login', [AuthenticateController::class, 'store'])->name('login.store');
 });
-
-Route::get('/register', [RegisterController::class, 'index'])->name('register.index');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
-
-Route::get('/login', [AuthenticateController::class, 'index'])->name('login');
-Route::post('/login', [AuthenticateController::class, 'store'])->name('login.store');
-
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
     Route::get('/product', [DashboardController::class, 'show'])->name('dashboard.show');
+});
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
+    Route::post('/add-to-cart/{book}', [CartController::class, 'addToCart'])->name('cart.store');
+    Route::delete('/delete/{cartItem}', [CartController::class, 'destroy'])->name('cart.destroy');
     Route::post('/logout', [AuthenticateController::class, 'logout'])->name('dashboard.logout');
+
+    Route::post('/checkout/{book}', [SaleController::class,'buyNow'])->name('sale.buyNow');
+    Route::post('/checkout', [SaleController::class,'buyCart'])->name('sale.buyCart');
+
     Route::get('/book', function () {
         return view('book/book');
     });
@@ -38,7 +49,6 @@ Route::get('cart', function () {
 
 Route::resource('categories', CategoryController::class);
 Route::resource('books', BookController::class);
-
 Route::resource('sales', SaleController::class);
 
 Route::get('/dashboard', function () {
